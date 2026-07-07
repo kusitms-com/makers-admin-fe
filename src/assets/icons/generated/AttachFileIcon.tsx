58023779cf5abcf18258ae041b6bd82cf069d5cd
Lee KyeongJoon <rudwns0224@naver.com>
2026-07-07 20:00:46 +0900

    feat: 첨부파일 아이콘 추가

---
+++ src/assets/icons/generated/AttachFileIcon.tsx
@@ -0,0 +1,16 @@
+// @generated
+import type { SVGProps } from 'react'
+const AttachFileIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" {...props}>
+    <mask id="a" width={20} height={20} x={0} y={0} maskUnits="userSpaceOnUse">
+      <path fill="currentColor" d="M0 0h20v20H0z" />
+    </mask>
+    <g mask="url(#a)">
+      <path
+        fill="currentColor"
+        d="M14.227 13.41q0 1.736-1.205 2.954t-2.934 1.218q-1.76 0-2.957-1.269-1.199-1.268-1.198-3.049V5.377q0-1.234.855-2.098t2.09-.864q1.255 0 2.1.916.846.915.846 2.191v7.404a1.7 1.7 0 0 1-.506 1.238 1.66 1.66 0 0 1-1.23.514q-.742 0-1.247-.534a1.82 1.82 0 0 1-.504-1.301V5.319H9.42v7.607q0 .282.19.475.19.194.47.194a.63.63 0 0 0 .47-.194.65.65 0 0 0 .19-.475V5.37a1.78 1.78 0 0 0-.538-1.324 1.8 1.8 0 0 0-1.328-.547q-.78 0-1.32.572-.538.57-.538 1.348v7.992q.008 1.282.899 2.175.89.893 2.173.913t2.177-.924.88-2.268V5.32h1.082z"
+      />
+    </g>
+  </svg>
+)
+export default AttachFileIcon
