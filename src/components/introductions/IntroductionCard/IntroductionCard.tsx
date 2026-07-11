b71a7e8e87211fe38c08791237e0538cbde92a7f
leemanjae02 <honthalg@gmail.com>
2026-07-11 14:35:19 +0900

    refactor: 재사용 입력 필드를 introductions에서 common으로 이동

--- src/components/introductions/IntroductionCard/IntroductionCard.tsx
+++ src/components/introductions/IntroductionCard/IntroductionCard.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@components/common/Button'
+import { Inputfield } from '@components/common/Inputfield'
 import { ImageBoxThumbnail } from '../ImageBoxThumbnail'
-import { Inputfield } from '../Inputfield'
 import { cn } from '@lib/utils'
 
 interface IntroductionCardProps {
