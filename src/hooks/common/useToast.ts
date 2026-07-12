f123cef9a231cbea338a2ebc6694b5665182c5e2
leemanjae02 <honthalg@gmail.com>
2026-07-12 22:58:40 +0900

    refactor: Toast 자동 dismiss가 removeToast를 재사용하도록 정리
    addToast의 setTimeout 콜백이 removeToast와 동일한 필터 로직을
    중복 구현하고 있어 get().removeToast(id) 호출로 통합

--- src/hooks/common/useToast.ts
+++ src/hooks/common/useToast.ts
@@ -15,13 +15,13 @@ interface ToastStore {
 
 const TOAST_DURATION_MS = 4000
 
-export const useToastStore = create<ToastStore>((set) => ({
+export const useToastStore = create<ToastStore>((set, get) => ({
   toasts: [],
   addToast: (toast) => {
     const id = crypto.randomUUID()
     set((state) => ({ toasts: [...state.toasts, { ...toast, id }] }))
     setTimeout(() => {
-      set((state) => ({ toasts: state.toasts.filter((item) => item.id !== id) }))
+      get().removeToast(id)
     }, TOAST_DURATION_MS)
   },
   removeToast: (id) => {
