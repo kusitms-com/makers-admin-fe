b435cb59730ae51dec2605c44aa402a1eef44d3a
leemanjae02 <honthalg@gmail.com>
2026-07-14 22:01:07 +0900

    fix: RootLayout 콘텐츠 padding이 PageHeader까지 밀어내던 문제 수정
    콘텐츠 padding을 main에 직접 주면 Outlet 안에서 페이지가 그리는
    PageHeader에도 적용돼 헤더 border-bottom이 안쪽으로 밀리는 문제가 있었음.
    padding은 페이지가 직접 콘텐츠 영역에 주도록 하고, PageHeader는 항상
    흰 배경을 갖도록 기본값으로 되돌림

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -5,11 +5,9 @@ export function RootLayout() {
   return (
     <div className="bg-fill-netural min-h-screen">
       <Sidebar className="fixed inset-y-0 left-0" />
-      <div className="flex min-h-screen flex-col pl-[220px]">
-        <main className="flex flex-1 flex-col gap-7 pt-6 pr-8 pb-[60px] pl-6">
-          <Outlet />
-        </main>
-      </div>
+      <main className="flex min-h-screen flex-col pl-[220px]">
+        <Outlet />
+      </main>
     </div>
   )
 }
